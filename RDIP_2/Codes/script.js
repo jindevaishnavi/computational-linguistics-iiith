9275c5a74531a3e28942ca1a9aebf3d0972449bb
jindevaishnavi <44129997+jindevaishnavi@users.noreply.github.com>
2020-07-19 17:31:11 +0530

    Goal-2 Hindi and English corpus added

---
+++ RDIP_2/Codes/script.js
@@ -0,0 +1,8 @@
+
+/*ENGLISH CORPUS*/
+english = ["The child liked the chocolate","She was stopped by the bravest knight",
+"Mary baked a cake for his birthday","She decorated the cake carefully","Mary wore a dress with polka dots"]
+
+/*HINDI CORPUS*/
+hindi = ["राम ने सीता के लिए फल तोड़ा।","छोटे बच्चे पाठशाला जल्दी आयेंगे।"
+,"मेहनत का फल मीठा होता है।","वाह! वह खूबसूरत है।","पेड़ से पत्ते गिर गए।"]
